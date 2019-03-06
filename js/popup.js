7a5c78ac75e3ab2939a7f0ddfb60172392a95991
RomK <romank1988@mail.ru>
2019-03-06 23:57:52 +0300

    Подготовка к защите

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,27 @@
+var linkCart = document.querySelector(".product-buy");
+var popupCart = document.querySelector(".popup-cart");
+var closeCart = popupCart.querySelector(".popup-close");
+
+linkCart.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupCart.classList.add("popup-show");
+});
+  
+closeCart.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupCart.classList.remove("popup-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+  evt.preventDefault();
+    if (popupCart.classList.contains("popup-show")) {
+      popupCart.classList.remove("popup-show");
+      popupCart.classList.remove("popup-error");
+    }
+  }
+});
+
 var linkLogin = document.querySelector(".contacts-button");
 var linkMap = document.querySelector(".map-link");
 var popupLogin = document.querySelector(".popup-login");
